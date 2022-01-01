64de7f5b005fda59d7f332ca7f24f250d9b7df9c
DuaaShykh <DuaaShykh>
2022-01-01 21:41:01 +0500

    Assignment1

---
+++ python_Asss.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a="Twinkle, twinkle, little star,"
+b="      How i wonder what you are!"
+c="            Up above the world so high"
+d="            Like a diamond in the sky."
+print(a)
+print(b)
+print(c)
+print(d)
+print(a)
+print(b)
+
+
+# In[2]:
+
+
+import sys
+print("python version")
+print(sys.version)
+
+
+# In[3]:
+
+
+import datetime
+now=datetime.datetime.now()
+print("current Date And Time!!")
+print (now)
+
+
+# In[ ]:
+
+
+from math import pi
+r=float(input("Enter radius of circle: "))
+area=pi*r**2 
+print("The area of circle is: ", area)
+
+
+# In[ ]:
+
+
+Firstname=input("Enter your Firstname: ")
+Lastname=input("Enter your lastname: ")
+print("The Full Name in Reverse Order is:  " + Lastname + " " + Firstname)
+
+
+# In[ ]:
+
+
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+add=a+b
+print("The addition of a and b is: ", add)
+
+
+# In[ ]:
+
+
+
+
